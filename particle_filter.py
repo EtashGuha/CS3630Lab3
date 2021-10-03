b7cfc6c09fb9b14d687098b2307b164f4f39e48b
Etash Guha <etashthebomb@gmail.com>
2021-10-02 20:36:41 -0400

    working

--- particle_filter.py
+++ particle_filter.py
@@ -1,9 +1,10 @@
 from grid import CozGrid
 from particle import Particle
-from utils import grid_distance, rotate_point, diff_heading_deg, add_odometry_noise
+from utils import grid_distance, rotate_point, diff_heading_deg, add_odometry_noise, add_gaussian_noise, add_marker_measurement_noise
 import setting
 import math
 import numpy as np
+import copy
 
 
 def motion_update(particles, odom, grid):
@@ -19,8 +20,15 @@ def motion_update(particles, odom, grid):
         Returns: the list of particles represents belief \tilde{p}(x_{t} | u_{t})
                 after motion update
     """
-    motion_particles = particles
-    return motion_particles
+    dx, dy, dh = odom
+    particlesList = []
+    for particle in particles:
+        x, y = rotate_point(dx, dy, particle.h)
+        particle.x += add_gaussian_noise(x, setting.ODOM_TRANS_SIGMA)
+        particle.y += add_gaussian_noise(y, setting.ODOM_TRANS_SIGMA)
+        particle.h += add_gaussian_noise(dh, setting.ODOM_HEAD_SIGMA)
+        particlesList.append(particle)
+    return particlesList
 
 # ------------------------------------------------------------------------
 def measurement_update(particles, measured_marker_list, grid):
@@ -47,7 +55,33 @@ def measurement_update(particles, measured_marker_list, grid):
         Returns: the list of particles represents belief p(x_{t} | u_{t})
                 after measurement update
     """
-    measured_particles = particles
-    return measured_particles
+    measuredMarkerListLength, num, weights = len(measured_marker_list), 0, []
+    
+    for particle in particles:
+        if measuredMarkerListLength:
+            robotMarkers = particle.read_markers(grid = grid)
+            markersLength = len(robotMarkers)
+            if not markersLength: weights.append(0)
+            elif not grid.is_free(x = particle.x, y = particle.y):
+                weights.append(0)
+                num += 1
+            else:
+                lst, confidence = [], 1
+                for measured_marker in measured_marker_list:
+                    if markersLength:
+                        worst = min(robotMarkers, key = lambda m: grid_distance(x1 = measured_marker[0], y1 = measured_marker[1], x2 = m[0], y2 = m[1]))
+                        robotMarkers.remove(worst)
+                        markersLength = len(robotMarkers)
+                        lst.append((measured_marker, worst))
+                for marker, worst in lst: confidence *= math.exp(-1 * (math.pow(grid_distance(marker[0], 
+                    marker[1], worst[0], worst[1]), 2) / (2 * math.pow(setting.MARKER_TRANS_SIGMA, 2)) 
+                + math.pow(diff_heading_deg(marker[2], worst[2]), 2) / (2 * math.pow(setting.MARKER_ROT_SIGMA, 2))))
+                weights.append(confidence)
+    particleLength, weightsSum = len(particles), sum(weights)
+    if measuredMarkerListLength < 1 or weightsSum == 0: weights = particleLength * [(1 / float(particleLength))]
+    else: weights = [weight / weightsSum for weight in weights]
+    beliefList = [Particle(x = particle.x, y = particle.y, heading = particle.h) for particle in np.random.choice(particles, size = (particleLength - min(particleLength, 50 + num)), p = weights).tolist()]
+    for x, y in [grid.random_free_place() for i in range(min(particleLength, 50 + num))]: beliefList.append(Particle(x = x, y = y))
+    return beliefList
 
 
